f15b24df92702c7423729d0fbe816eba89caae84
yuya-h-29 <tzou11291996@gmail.com>
2024-02-10 20:05:22 +0900

    Initial Commit

---
+++ WorldLandmarks/WorldLandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorldLandmarksApp.swift
+//  WorldLandmarks
+//
+//  Created by Yuya Harada on 2024/02/10.
+//
+
+import SwiftUI
+
+@main
+struct WorldLandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
